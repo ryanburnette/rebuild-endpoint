ee329a66dfd2590a81e698fe7ff37c6ff05464a1
Ryan Burnette <ryan.burnette@gmail.com>
2020-05-10 12:24:28 -0400

    1.0.2
    - add license
    - update .prettierrc
    - no material changes

--- index.js
+++ index.js
@@ -28,15 +28,9 @@ module.exports = function (opts) {
     var payload = JSON.stringify(req.body);
     var sig = req.get('X-Hub-Signature');
     var hmac = crypto.createHmac('sha1', opts.secret);
-    var digest = Buffer.from(
-      'sha1=' + hmac.update(payload).digest('hex'),
-      'utf8'
-    );
+    var digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8');
     var checksum = Buffer.from(sig, 'utf8');
-    if (
-      checksum.length !== digest.length ||
-      !crypto.timingSafeEqual(digest, checksum)
-    ) {
+    if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
       return res.sendStatus(401);
     }
 
